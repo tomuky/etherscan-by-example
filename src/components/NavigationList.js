1631f4b1228ad4e7a2aec33b41183157e793b6f9
Tom Davis <tom@Toms-MacBook-Pro.local>
2023-10-24 13:22:33 -0400

    new socials

--- src/components/NavigationList.js
+++ src/components/NavigationList.js
@@ -34,8 +34,8 @@ const NavigationList = (props) => {
 
             <div className={classes.left_pane_divider}/>
 
-            <NavigationLinkExternal to='https://twitter.com/tomuky' title='Built by @tomuky' icon='twitter-logo.png'/>
-            <NavigationLinkExternal to='https://github.com/tomuky/etherscan-by-example' title='etherscan-by-example' icon='github-mark-white.png'/>
+            <NavigationLinkExternal to='https://twitter.com/etherscan-by-ex' title='Follow' icon='twitter-logo.png'/>
+            <NavigationLinkExternal to='https://github.com/tomuky/etherscan-by-example' title='Contribute' icon='github-mark-white.png'/>
             {/* <NavigationLinkExternal to='mailto:tom@fastdefitutorial.com' title='Email @tomuky' icon='email-icon.png'/> */}
 
         </div>
